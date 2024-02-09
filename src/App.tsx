c2f99b0e413f7442e95fc7d5a391ab18be1e58dc
Muhammad-Huzaifa15180 <mh335130@gmail.com>
2024-02-09 21:26:57 +0500

    created layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-
-import Feed from './components/Feed'
-import Rightbar from './components/Rightbar'
-import Navbar from './components/Navbar'
-import './App.css'
-import Sidebar from './components/Sidebar'
-import { Box, Stack } from '@mui/material'
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { Box, Grid } from "@mui/material";
 
 function App() {
-
   return (
-   <Box>
-    {/* <Navbar/> */}
-    <Stack direction="row" spacing={2} alignItems="center" justifyContent="space-between">
-    <Sidebar/>
-    <Feed />
-    <Rightbar />
-    </Stack>
+    <Box>
+      <Navbar />
+      <Grid container position="relative" height="calc(100% - 64px)">
+        <Grid xs={3} position="fixed" top={50}>
+          <Sidebar />
+        </Grid>
+        <Grid
+          xs={6}
+          position="absolute"
+          right="30%"
+          top={40}
+          // sx={{ overflowY: "scroll" }}
+        >
+            <Feed />
+          
+        </Grid>
+        <Grid xs={3} position="absolute" right="10%" top={40}>
+          <Rightbar />
+        </Grid>
+      </Grid>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
